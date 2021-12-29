015c21895acf0bb932faa06bea4af2b9be814bb1
11carmel11 <11carmrl11@gmail.com>
2021-12-29 17:26:27 +0200

    seperated components

---
+++ 35.socket.io-docker/client/src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function List({ list }) {
+  return (
+    <ul style={{ border: "5px solid red", display: "inline-grid" }}>
+      {list.map((person, i) => (
+        <li key={i}>{person}</li>
+      ))}
+    </ul>
+  );
+}

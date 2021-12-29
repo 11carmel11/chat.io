b04bfd21fe279187b71b3a285682ca2959a6b863
11carmel11 <11carmrl11@gmail.com>
2021-12-29 16:46:42 +0200

    added logged list

--- 35.socket.io-docker/client/src/components/Chat.jsx
+++ 35.socket.io-docker/client/src/components/Chat.jsx
@@ -1,23 +1,42 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState, useContext } from "react";
 import TextField from "@material-ui/core/TextField";
 import io from "socket.io-client";
 import { nanoid } from "nanoid";
+import axios from "axios";
 import notyf from "../notyf";
 import nameContext from "../contexts/name/context";
+
+const server = "http://localhost:4000";
 
 export default function Chat() {
   const { name } = useContext(nameContext);
 
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
+  const [list, setList] = useState([]);
 
   const socketRef = useRef();
 
   useEffect(() => {
-    socketRef.current = io.connect("http://localhost:4000");
+    axios.post(`${server}?name=${name}`);
 
-    socketRef.current.on("messageBack", (socket) => {
-      setChat((prevState) => [...prevState, socket]);
+    socketRef.current = io.connect(server);
+
+    socketRef.current.on("loggedList", (loggedList) => {
+      setList(loggedList);
+    });
+
+    socketRef.current.on("joined", (person) => {
+      notyf.success(`${person} has just joined the chat`);
+    });
+
+    socketRef.current.on("disconnected", (person) => {
+      notyf.success(`${person} has just left the chat`);
+    });
+
+    socketRef.current.on("messageBack", (messageObj) => {
+      setChat((prevState) => [...prevState, messageObj]);
     });
     // return () => socketRef.current.disconnect();
   }, []);
@@ -66,6 +85,11 @@ export default function Chat() {
         <h1>Chat Log</h1>
         {renderChat()}
       </div>
+      <ul style={{ border: "5px solid red", display: "inline-grid" }}>
+        {list.map((person, i) => (
+          <li key={i}>{person}</li>
+        ))}
+      </ul>
     </div>
   );
 }

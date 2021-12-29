90c70ad883403c33c65e071eea1c752556b286c8
11carmel11 <11carmrl11@gmail.com>
2021-12-29 12:27:50 +0200

    init

---
+++ 35.socket.io-docker/server/index.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  socket.on("message", ({ name, message }) => {
+    io.emit("messageBack", { name, message });
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("messageBack", { name: "wow", message: "render" });
+  });
+});
+
+http.listen(4000, () => {
+  console.log("listening on port 4000");
+});
